c99fecc74deded2ed3be7c461800528b8055709b
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-21 09:00:53 +0200

    conatct form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">Hero</div>
+  )
+}
+
+export default Hero
