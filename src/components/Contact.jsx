869c25ba8d30bec25669f510308a184ef9c2356d
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-21 15:15:35 +0200

    contact form reset

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">Contact</div>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+   
+ 
+    </div>
   )
 }
 
